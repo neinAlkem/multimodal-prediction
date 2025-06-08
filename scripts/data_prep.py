f86c3ef762a8fbff6077fbda5588ef4ed285ed37
NeinAlkem <bagasrizal.work@gmail.com>
2025-06-08 05:13:10 +0000

    pipeline : add spark cluster

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -3,12 +3,18 @@ from pyspark.sql import functions as F
 import logging
 from pyspark.ml.feature import StringIndexer
 import sys
+import os
+import io
+from dotenv import load_dotenv
+from typing import Tuple
+
+load_dotenv()
 
 logging.basicConfig( format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.DEBUG)
 
-def data_prep(spark:SparkSession, data_gcs:str) -> DataFrame :
+def data_prep(spark:SparkSession, data_gcs:str) -> Tuple[DataFrame, DataFrame]:
     
     logging.info(f"Starting Data Preparation Process...")
     logging.info(f"Path info: {data_gcs}")
@@ -41,6 +47,8 @@ def data_prep(spark:SparkSession, data_gcs:str) -> DataFrame :
 # ---------------------------------------------------------------------------- #
     train_data, test_data = df.randomSplit([0.8, 0.2], seed=123)
     
+    logging.info(f'Training data row: {train_data.count()}')
+    logging.info(f'Training data row: {test_data.count()}')
     return train_data, test_data
 
 if __name__ == "__main__":
@@ -53,23 +61,50 @@ if __name__ == "__main__":
     train_data_gcs_path = sys.argv[3]
     test_data_gcs_path = sys.argv[4]
     
-    spark = SparkSession.builder.appName('data_pipeline').getOrCreate()
-
-    logging.info(f'Spark Session Started. Using Project: {project_id_env}')
+    spark = SparkSession \
+    .builder \
+    .master('spark://localhost:7077') \
+    .config("spark.jars.packages", "com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.36.1") \
+    .config("spark.jars", "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar") \
+    .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
+    .config("spark.hadoop.fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS") \
+    .config("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
+     .config("spark.hadoop.fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem") \
+     .config("spark.hadoop.fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS") \
+     .config("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
+     .config("spark.hadoop.google.cloud.auth.type", "SERVICE_ACCOUNT_JSON_KEYFILE") \
+     .config("spark.hadoop.fs.gs.project.id", os.getenv('PROJECT_ID')) \
+     .appName("project_spark") \
+     .getOrCreate()
+     
     logging.info(f'Fusion Data Dir: {fusion_data_gcs_path}')
     logging.info(f'Training Data Dir: {train_data_gcs_path}')
     logging.info(f'Testing Dir: {test_data_gcs_path}')
     
-    train_data, test_data = data_prep(spark, 
-                   fusion_data_gcs_path)
-    
-    logging.info(f"Saving Training Data To: {train_data_gcs_path}")
-    train_data.write.mode("overwrite").option("header", "true").csv(train_data_gcs_path)
-    logging.info("Training Data saved successfully to GCS as CSV.")
-    
-    logging.info(f"Saving Testing Data To: {test_data_gcs_path}")
-    test_data.write.mode("overwrite").option("header", "true").csv(train_data_gcs_path)
-    logging.info("Testing Data saved successfully to GCS as CSV.")
+    try:
+        train_data, test_data = data_prep(spark, 
+                       fusion_data_gcs_path)
+        
+        df_train_data = train_data.toPandas()
+        df_test_data = test_data.toPandas()
+
+        logging.info(f"Saving Training Data To: {train_data_gcs_path}")
+        df_train_data.to_csv(train_data_gcs_path, 
+                             index=False, 
+                             header=True, 
+                             mode='w')
+        logging.info("Training Data saved successfully to GCS as CSV.")
+
+        logging.info(f"Saving Testing Data To: {test_data_gcs_path}")
+        df_test_data.to_csv(test_data_gcs_path, 
+                            index=False, 
+                            header=True, 
+                            mode='w')
+        logging.info("Testing Data saved successfully to GCS as CSV.")
+    except Exception as e:
+        logging.error(f'Error during data preparation process: {e}')
+    finally:
+        spark.stop()
 
 
     
