a89013b987d2a1c7249ee280777aa8cd98d67d9f
NeinAlkem <bagasrizal.work@gmail.com>
2025-06-09 09:52:12 +0000

    chore : remove unused file

--- model/model_registry.sh
+++
@@ -1,8 +0,0 @@
-export PROJECT_ID="project-big-data-461104"
-export REGION="asia-southeast1"
-export REPO_NAME="model-images"
-export IMAGE_NAME="engagement-classifier-trainer"
-export IMAGE_TAG="v1.0"
-export IMAGE_URI="${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG}"
-
-gcloud builds submit --tag ${IMAGE_URI} .
